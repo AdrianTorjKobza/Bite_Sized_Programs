311a15f76e4e9deae593af1c5026f186a32805d3
Adrian Torj-Kobza <42949599+AdrianTorjKobza@users.noreply.github.com>
2020-01-25 22:25:31 +0200

    Create main.py

---
+++ Python/Daily_Coding_Problem/Problem #22 [Medium]/main.py
@@ -0,0 +1,33 @@
+import itertools
+
+words = ["bed", "bath", "bedbath", "and", "beyond"]
+words.sort()
+
+string = "bedbathandbeyond"
+
+# Naive solution
+output_list = []
+word = ""
+
+for s in string:
+    word = word + s
+
+    if word in words:
+        output_list.append(word)
+        word = ""
+
+print (output_list)
+
+# Complete solution
+all_words_combinations = []
+output_list = []
+
+for n in range(len(words)):
+    words_combination = itertools.permutations(words, n)
+    all_words_combinations = all_words_combinations + list(words_combination)
+
+for c in all_words_combinations:
+    if ''.join(c) == string:
+        output_list.append(list(c))
+
+print (output_list)
