dbdd152434befcc1f4e9d7634a0f8c52bbec287c
Adrian Torj-Kobza <42949599+AdrianTorjKobza@users.noreply.github.com>
2020-01-29 15:44:00 +0200

    Create main.py

---
+++ Python/Daily_Coding_Problem/Problem #21 [Easy]/main.py
@@ -0,0 +1,44 @@
+# Given an array of time intervals (start, end) for classroom lectures (possibly overlapping), find the minimum number of rooms required.
+# For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
+
+time = [(30, 75), (0, 50), (60, 150), (90, 120), (10, 20), (0, 200)]
+time.sort()
+
+# List of max amount of rooms needed.
+rooms = []
+for i in range(len(time)):
+    rooms.append([])
+# Output: [[], [], [], [], [], []] - 6 empty lists / rooms inside one list.
+
+def add_time_in_rooms (time):
+    # Iterate over the rooms lists.
+    for i in range(len(rooms)):
+        flag = True
+        
+        # Check if current room is not empty.
+        if len(rooms[i]) > 0:
+            # Iterate over the time slots from the current room.
+            for j in range(len(rooms[i])):
+                # If we have an overlap set the flag to False. 
+                if time[0][0] < rooms[i][j][1]:
+                    flag = False
+            
+            # If we have no overlap, add the time slot in the current room. 
+            if flag == True:
+                rooms[i].append(time[0])
+                break
+        # If a room is empty, add the time slot in the room.
+        else:
+            rooms[i].append(time[0])
+            break
+    
+    # Remove the element from the time list. 
+    time.pop(0)
+    
+# Call the function, until we remove all the time slots from the list.
+while len(time) > 0:
+    add_time_in_rooms (time)
+
+# Display the time slots for each room.
+for i in range (0, len(rooms)):
+    print ("ROOM " + str(i) + ":" + str(rooms[i]))
